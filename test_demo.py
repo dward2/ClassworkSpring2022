8057fb0c9edcfa2c436985f674ad598769ba6ad8
David Ward <david.a.ward@duke.edu>
2022-02-02 10:34:43 -0500

    Files for testing

---
+++ test_demo.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    [0.1, 0.2, 0.3],
+    ])
+def test_add(a, b, expected):
+    from demo import add
+    answer = add(a, b)
+    assert answer == pytest.approx(expected)
---
+++ test_weight_entry.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("22 lb", 10),
+    ("50 kg", 50),
+    ("22.1 lb", 10),
+    ("38.9 kg", 39),
+    ("22 lbs", 10),
+    ("22 LB", 10),
+    ("22 lb", 10),
+    ("ten kg", 10),
+    ("-22 lb", -10),
+    ])
+def test_parse_weight_input(input, expected):
+    from weight_entry import parse_weight_input
+    answer = parse_weight_input(input)
+    assert answer == expected
